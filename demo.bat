321c3b89893cbfc7caecd3418927e7b275ce1249
Kamil <kamil.szydlowski.stud@pw.edu.pl>
2022-05-03 22:56:10 +0200

    Add ReadMe.md

--- demo.bat
+++
@@ -1,9 +0,0 @@
-curl -X GET localhost:8080/screenings^
-   -H "Content-Type: application/json"^
-   -d "{\"yyyyMMDD\": \"20220501\", \"startHHMM\": \"0000\", \"endHHMM\": \"2359\"}"
-
-curl -X GET localhost:8080/screening/6
-
-curl -X GET localhost:8080/booking/6^
-    -H "Content-Type: application/json"^
-    -d "{\"seatSelectionRequests\": [{\"line\": 2, \"column\": 2, \"ticket\": \"adult\"}], \"firstName\": \"Kamil\", \"lastName\": \"Szydlowski\"}"
