7d4e6b0c4baed91ba2c6eefae847a88b62658b16
Kamil Szydłowski <kamil.szydlowski.stud@pw.edu.pl>
2022-05-03 21:36:32 +0200

    Add films sorting

--- demo.bat
+++ demo.bat
@@ -1,6 +1,6 @@
 curl -X GET localhost:8080/screenings^
    -H "Content-Type: application/json"^
-   -d "{\"yyyyMMDD\": \"20220501\", \"startHHMM\": \"1400\", \"endHHMM\": \"1630\"}"
+   -d "{\"yyyyMMDD\": \"20220501\", \"startHHMM\": \"0000\", \"endHHMM\": \"2359\"}"
 
 curl -X GET localhost:8080/screening/6
 
--- src/main/java/com/kszydlo1/ticket_booking_app/views/ScreeningsView.java
+++ src/main/java/com/kszydlo1/ticket_booking_app/views/ScreeningsView.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Vector;
 import java.util.stream.Collectors;
@@ -27,20 +28,9 @@ public class ScreeningsView {
             if (sp.getStartDate().after(sp.getEndDate()))
                 throw new StartEndDateException();
 
-            Vector response = new Vector<ScreeningsPeriodResponse>();
-            List<Screening> screenings = (List<Screening>) screeningRepository.findAll()
-                    .stream()
-                    .filter(screening -> screening.getStartTime().after(sp.getStartDate())
-                            && screening.getStartTime().before(sp.getEndDate()))
-                    .collect(Collectors.toList());
-            for (Screening screening : screenings) {
-                ScreeningsPeriodResponse screeningsPeriodResponse = new ScreeningsPeriodResponse();
-                screeningsPeriodResponse.setScreeningId(screening.getScreeningId());
-                screeningsPeriodResponse.setTitle(screening.getMovie().getTitle());
-                screeningsPeriodResponse.setStartTime(screening.getStartTime());
+            Vector response = getResponse(sp);
+            response = getSortedResponse(response);
 
-                response.add(screeningsPeriodResponse);
-            }
             return response;
         }
         catch (StartEndDateException e) {
@@ -48,5 +38,38 @@ public class ScreeningsView {
             response.add(Constants.Views.START_END_DATE_EXCEPTION);
             return response;
         }
+    }
+
+    private Vector getResponse(ScreeningsPeriodRequest sp) {
+        Vector response = new Vector<ScreeningsPeriodResponse>();
+        List<Screening> screenings = (List<Screening>) screeningRepository.findAll()
+                .stream()
+                .filter(screening -> screening.getStartTime().after(sp.getStartDate())
+                        && screening.getStartTime().before(sp.getEndDate()))
+                .collect(Collectors.toList());
+        for (Screening screening : screenings) {
+            ScreeningsPeriodResponse screeningsPeriodResponse = new ScreeningsPeriodResponse();
+            screeningsPeriodResponse.setScreeningId(screening.getScreeningId());
+            screeningsPeriodResponse.setTitle(screening.getMovie().getTitle());
+            screeningsPeriodResponse.setStartTime(screening.getStartTime());
+
+            response.add(screeningsPeriodResponse);
+        }
+        return response;
+    }
+
+    private Vector getSortedResponse(Vector unsortedVec) {
+        Vector sortedVec = new Vector<>(unsortedVec);
+        sortedVec.sort(new Comparator<ScreeningsPeriodResponse>() {
+            @Override
+            public int compare(ScreeningsPeriodResponse s1, ScreeningsPeriodResponse s2) {
+                if(s1.getTitle() == s2.getTitle()){
+                    return s1.getStartTime().compareTo(s2.getStartTime());
+                }
+                else
+                    return s1.getTitle().compareTo(s2.getTitle());
+            }
+        });
+        return sortedVec;
     }
 }
